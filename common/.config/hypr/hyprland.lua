9eb4ba06ccde948c94b6eea5e0dcac4640c5f0f2
athinaslime <athinaslime@gmail.com>
2026-06-03 20:57:37 +0300

    feat(hyprland): add autostart configuration

--- common/.config/hypr/hyprland.lua
+++ common/.config/hypr/hyprland.lua
@@ -2,6 +2,8 @@ require("landmodules.input")
 require("landmodules.animations")
 require("landmodules.misc")
 
+require("landmodules.autostart")
+require("landmodules.binds")
 require("landmodules.env-variables")
 require("landmodules.monitors")
 require("landmodules.rules")
